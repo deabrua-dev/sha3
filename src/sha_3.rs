496f9bb062e037cd9326e6109611b7d7ba4bec09
deabrua-dev <deabrua.co@gmail.com>
2023-11-05 19:28:43 +0200

    Initial commit. Add sha3 and keccak implementations.

---
+++ src/sha_3.rs
@@ -0,0 +1,164 @@
+pub enum SHA3 {
+    SHA3_224,
+    SHA3_256,
+    SHA3_384,
+    SHA3_512,
+    KECCAK_224,
+    KECCAK_256,
+    KECCAK_384,
+    KECCAK_512
+}
+
+const ROTATION_OFFSET: [[u32; 5]; 5] = [
+    [0, 36, 3, 41, 18],
+    [1, 44, 10, 45, 2],
+    [62, 6, 43, 15, 61],
+    [28, 55, 25, 21, 56],
+    [27, 20, 39, 8, 14],
+];
+
+const RND_C: [u64; 24] = [
+    0x0000000000000001, 
+    0x0000000000008082, 
+    0x800000000000808A,
+    0x8000000080008000, 
+    0x000000000000808B, 
+    0x0000000080000001,
+    0x8000000080008081, 
+    0x8000000000008009, 
+    0x000000000000008A,
+    0x0000000000000088, 
+    0x0000000080008009, 
+    0x000000008000000A,
+    0x000000008000808B, 
+    0x800000000000008B, 
+    0x8000000000008089,
+    0x8000000000008003, 
+    0x8000000000008002, 
+    0x8000000000000080,
+    0x000000000000800A, 
+    0x800000008000000A, 
+    0x8000000080008081,
+    0x8000000000008080, 
+    0x0000000080000001, 
+    0x8000000080008008
+];
+
+const RATE_ARRAY: [u64; 4] = [ 1152, 1088, 832, 576 ];
+const CAPACITY_ARRAY: [u64; 4] = [ 448, 512, 768, 1024 ];
+
+const ROUNDS: usize = 24;
+
+fn round_b(A: &mut Vec<Vec<u64>>) {
+    for i in 0..ROUNDS {
+        let mut C = [0; 5];
+        let mut D = [0; 5];
+        let mut B = [[0; 5]; 5];
+
+        // Theta Step
+        for x in 0..5 {
+            C[x] = A[x][0] ^ A[x][1] ^ A[x][2] ^ A[x][3] ^ A[x][4];
+        }
+        for x in 0..5 {
+            D[x] = C[(x + 4) % 5] ^ C[(x + 1) % 5].rotate_left(1);
+        }
+        for x in 0..5 {
+            for y in 0..5 {
+                A[x][y] ^= D[x];
+            }
+        }
+        // Rho and Pi Steps
+        for x in 0..5 {
+            for y in 0..5 {
+                B[y][(2 * x + 3 * y) % 5] = A[x][y].rotate_left(ROTATION_OFFSET[x][y]);
+            }
+        }
+        // Chi Step
+        for x in 0..5 {
+            for y in 0..5 {
+                A[x][y] = B[x][y] ^ ((!B[(x + 1) % 5][y]) & B[(x + 2) % 5][y])
+            }
+        }
+        // Iota step
+        A[0][0] ^= RND_C[i];
+    }
+}
+
+fn keccak_f1600(state: &mut Vec<u8>) {
+    let mut lanes: Vec<Vec<u64>>  = vec![];
+    for x in 0..5 {
+        let mut temp: Vec<u64> = vec![];
+        for y in 0..5 {
+            temp.push(u64::from_le_bytes(state[(8 * (x + 5 * y))..(8 * (x + 5 * y) + 8)].try_into().unwrap()));
+        }
+        lanes.push(temp);
+    }
+    round_b(&mut lanes);
+    for x in 0..5 {
+        for y in 0..5 {
+            let temp = lanes[x][y].to_le_bytes();
+            for (i, &t) in temp.iter().enumerate() {
+                state[8 * (x + 5 * y) + i] = t;
+            }
+        }
+    }
+}
+
+pub fn keccak(input_bytes: &[u8], length: SHA3) -> Result<Vec<u8>, String> {
+    let input_bytes_len = input_bytes.len();
+
+    let rcd = match length {
+        SHA3::SHA3_224 => (RATE_ARRAY[0], CAPACITY_ARRAY[0], 0x06),
+        SHA3::SHA3_256 => (RATE_ARRAY[1], CAPACITY_ARRAY[1], 0x06),
+        SHA3::SHA3_384 => (RATE_ARRAY[2], CAPACITY_ARRAY[2], 0x06),
+        SHA3::SHA3_512 => (RATE_ARRAY[3], CAPACITY_ARRAY[3], 0x06),
+        SHA3::KECCAK_224 => (RATE_ARRAY[0], CAPACITY_ARRAY[0], 0x01),
+        SHA3::KECCAK_256 => (RATE_ARRAY[1], CAPACITY_ARRAY[1], 0x01),
+        SHA3::KECCAK_384 => (RATE_ARRAY[2], CAPACITY_ARRAY[2], 0x01),
+        SHA3::KECCAK_512 => (RATE_ARRAY[3], CAPACITY_ARRAY[3], 0x01),
+    };
+    let rate = rcd.0;
+    let capacity = rcd.1;
+    let delimiter = rcd.2;
+
+    if rate + capacity != 1600 || rate % 8 != 0 {
+        return Err(String::from("Error"));
+    }
+    let rates_in_bytes = (rate / 8) as usize;
+
+    let mut output_bytes_len = (capacity / 16) as usize;
+
+    let mut output_bytes = vec![];
+    let mut state = vec![0; 200];
+    
+    let mut input_offset = 0;
+    let mut block_size = 0;
+
+    while input_offset < input_bytes_len {
+        block_size = std::cmp::min(input_bytes_len - input_offset, rates_in_bytes);
+        for i in 0..block_size {
+            state[i] ^= input_bytes[i + input_offset];
+        }
+        input_offset += block_size;
+        if block_size == rates_in_bytes {
+            keccak_f1600(&mut state);
+        }
+    }
+    state[block_size] ^= delimiter;
+
+    if (delimiter & 0x80) != 0 && (block_size == rates_in_bytes - 1) {
+        keccak_f1600(&mut state);
+    }
+    state[rates_in_bytes - 1] ^= 0x80;
+    keccak_f1600(&mut state);
+
+    while output_bytes_len > 0 {
+        block_size = std::cmp::min(output_bytes_len, rates_in_bytes as usize);
+        output_bytes.append(&mut state[0..block_size].to_vec());
+        output_bytes_len -= block_size;
+        if output_bytes_len > 0 {
+            keccak_f1600(&mut state);
+        }
+    }
+    Ok(output_bytes)
+}
---
+++ src/tests.rs
@@ -0,0 +1,72 @@
+#[cfg(test)]
+mod test {
+    use std::result;
+
+    use crate::sha_3::*;
+    use hex::encode;
+
+    
+    #[test]
+    fn sha3_224_test() {
+        let text = b"Hello world!";
+        let precompiled_sha3_output = "d3ee9b1ba1990fecfd794d2f30e0207aaa7be5d37d463073096d86f8";
+        let result = keccak(text, SHA3::SHA3_224).unwrap();
+        assert_eq!(precompiled_sha3_output, encode(result));
+    }
+
+    #[test]
+    fn sha3_256_test() {
+        let text = b"Twinkling Watermelon";
+        let precompiled_sha3_output = "e9419f6cd10ee5fb4ee12468f94613b8c25bb75acea2ac8b913c324b6bbd4db3";
+        let result = keccak(text, SHA3::SHA3_256).unwrap();
+        assert_eq!(precompiled_sha3_output, encode(result));
+    }
+
+    #[test]
+    fn sha3_384_test() {
+        let text = b"33ced2c76b26cae94e162c4c0d2c0ff7c13094b0185a3c122e732d5ba77efebc";
+        let precompiled_sha3_output = "12bd50d9444609624b6bff13140b0c477a7a45300691744cc7f6af8ebbf66404f22c19c21c1432438f26a5ec5d044a9a";
+        let result = keccak(text, SHA3::SHA3_384).unwrap();
+        assert_eq!(precompiled_sha3_output, encode(result));
+    }
+
+    #[test]
+    fn sha3_512_test() {
+        let text = b"33ced2c76b26cae94e162c4c0d2c0ff7c13094b0185a3c122e732d5ba77efebc33ced2c76b26cae94e162c4c0d2c0ff7c13094b0185a3c122e732d5ba77efebc33ced2c76b26cae94e162c4c0d2c0ff7c13094b0185a3c122e732d5ba77efebc";
+        let precompiled_sha3_output = "d86f5dd6ce4c0f4e6a8010a35f9f8bb8db3e4db578897146a07b24f7c4ab267b704a2f37b7043680c26be8168419a91c926f41e5cbb63eb639d3fcd15cdf90a3";
+        let result = keccak(text, SHA3::SHA3_512).unwrap();
+        assert_eq!(precompiled_sha3_output, encode(result));
+    }
+
+    #[test]
+    fn keccak_224_test() {
+        let text = b"Hello world!";
+        let precompiled_keccak_output = "2528dc7b1a2c2c45d31ba2e5b21d46f78558160ef046a04df20e2233";
+        let result = keccak(text, SHA3::KECCAK_224).unwrap();
+        assert_eq!(precompiled_keccak_output, encode(result));
+    }
+
+    #[test]
+    fn keccak_256_test() {
+        let text = b"Twinkling Watermelon";
+        let precompiled_keccak_output = "e853c0d2c020b013bfff1d214238e2d0d9f96fed3aad12a7a65a15028662c653";
+        let result = keccak(text, SHA3::KECCAK_256).unwrap();
+        assert_eq!(precompiled_keccak_output, encode(result));
+    }
+
+    #[test]
+    fn keccak_384_test() {
+        let text = b"33ced2c76b26cae94e162c4c0d2c0ff7c13094b0185a3c122e732d5ba77efebc";
+        let precompiled_keccak_output = "68ca5f8547d34f5435abf048c70e49c4e7d4bea205d345599c673bd4239681834aec9f3edb6fe3962b400796eb2c28db";
+        let result = keccak(text, SHA3::KECCAK_384).unwrap();
+        assert_eq!(precompiled_keccak_output, encode(result));
+    }
+
+    #[test]
+    fn keccak_512_test() {
+        let text = b"33ced2c76b26cae94e162c4c0d2c0ff7c13094b0185a3c122e732d5ba77efebc33ced2c76b26cae94e162c4c0d2c0ff7c13094b0185a3c122e732d5ba77efebc33ced2c76b26cae94e162c4c0d2c0ff7c13094b0185a3c122e732d5ba77efebc";
+        let precompiled_keccak_output = "60e91fad62a9492da46b5916d19d2251491600d0e37a3aaa6ecc4b2e582b2b07d42fc4d235be9adcaaad5c2489b9b7d6eec9dad14d54967d43d832d30ada15ed";
+        let result = keccak(text, SHA3::KECCAK_512).unwrap();
+        assert_eq!(precompiled_keccak_output, encode(result));
+    }
+}
